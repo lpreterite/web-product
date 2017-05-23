7f52a4c20e6c1d302e52a3941a6f8c6be15cfdf1
packy <lpreterite@126.com>
2017-05-23 11:09:08 +0800

    init

---
+++ template/README.md
@@ -0,0 +1,24 @@
+# {{ name }}
+
+> {{ description }}
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# build for development in {{ output }} folder
+npm run dev
+
+# build and watch for development in {{ output }} folder
+npm run watch
+
+# serve with hot reload at localhost:8080
+npm run hot
+
+# build for production with minification
+npm run production
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ template/webpack.mix.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const { mix } = require('laravel-mix');
+mix.options({notifications: false});
+mix.webpackConfig({
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            }
+        ]
+    },
+    devtool: 'source-map',
+    devServer: {
+        contentBase: path.join(__dirname, "{{ output }}")
+    }
+});
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('src/main.js', '{{ output }}/assets/js/')
+   .sass('src/style.scss', '{{ output }}/assets/css/')
+   .options({
+        processCssUrls: false
+   })
+   .extract([
+        'vue'
+    ]);
+
+mix
+    .copy('src/index.html', '{{ output }}/')
+    .copyDirectory('src/assets', '{{ output }}/assets/');
